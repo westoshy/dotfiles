a383f23dc8e0c5275fdd61e9fe195bac9be88ba2
westoshy <t.nishimura020830@gmail.com>
2015-02-02 15:33:42 +0900

    add vsvimrc

--- setup.bat
+++ setup.bat
@@ -1,6 +1,8 @@
 mklink %HOME%\.vimrc %HOME%\dotfiles\.vimrc
 mklink %HOME%\.gvimrc %HOME%\dotfiles\.gvimrc
+mklink %HOME%\.vsvimrc %HOME%\dotfiles\.vsvimrc
 mklink %HOME%\.bashrc %HOME%\dotfiles\.bashrc
 mklink %HOME%\.minttyrc %HOME%\dotfiles\.minttyrc
 mklink %HOME%\.bash_profile %HOME%\dotfiles\.bash_profile
 mklink %HOME%\.zshrc %HOME%\dotfiles\.zshrc
+pause
